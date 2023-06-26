694d03945d139ca26dbb705ee309a820e889c87b
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-06-26 10:17:57 +0200

    Refactor 'Program.cs'

--- PageCounter/FileManager.cs
+++ PageCounter/FileManager.cs
@@ -1,4 +1,5 @@
-﻿using PageCounter.ConsoleUtils;
+﻿using Figgle;
+using PageCounter.ConsoleUtils;
 using System;
 using System.Diagnostics;
 using System.IO;
@@ -7,19 +8,10 @@ namespace PageCounter
 {
     public class FileManager
     {
-        private string ApplicationTitleArt = @"
-
-██████   █████   ██████  ███████      ██████  ██████  ██    ██ ███    ██ ████████ ███████ ██████  
-██   ██ ██   ██ ██       ██          ██      ██    ██ ██    ██ ████   ██    ██    ██      ██   ██ 
-██████  ███████ ██   ███ █████       ██      ██    ██ ██    ██ ██ ██  ██    ██    █████   ██████  
-██      ██   ██ ██    ██ ██          ██      ██    ██ ██    ██ ██  ██ ██    ██    ██      ██   ██ 
-██      ██   ██  ██████  ███████      ██████  ██████   ██████  ██   ████    ██    ███████ ██   ██ 
-";
         private string Description = "A pdf file page counter that appends the count to the name of the pdf :)";
         private string ApplicationTitle = "PDF Page Counter";
 
-        private string FilePath = @"C:\PageCount";
-        private string Extension = ".PDF";
+        private string FilePath = @"C:\MFTemp1";
 
         // Simple constructor
         public FileManager()
@@ -30,54 +22,43 @@ namespace PageCounter
         // Entry of the file application, handles all the logic of the application
         public void Start()
         {
-            Console.Title = ApplicationTitle;
-            Console.WriteLine(ApplicationTitleArt);
+            Console.Title = $"{ApplicationTitle} {AddVersionNumber()}";
+            Console.WriteLine(FiggleFonts.Standard.Render(ApplicationTitle));
             Console.WriteLine(Description);
 
             WaitForKeyPress();
 
-            // TODO: Prompt the user for the location of the pdf files to rename
+            // TODO: prompt the user for the location of the pdf files to rename
+
+            NotifyUser("(processing...)");
 
             // Cache the files in the location
-            try
+            FileInfo[] files = FileOperations.GetFiles(FilePath);
+
+            int counter = 1;
+
+            foreach (FileInfo file in files) 
             {
-                FileInfo[] files = FileUtils.GetFiles(FilePath);
-                int counter = 1;
-                foreach (FileInfo file in files) 
+                if (false == Path.GetFileNameWithoutExtension(file.FullName).Contains("["))
                 {
-                    if (false == Path.GetFileNameWithoutExtension(file.FullName).Contains("["))
-                    {
-                        // Get file count
-                        int pageCount = FileUtils.GetNumberOfPages(file.FullName);
+                    // Get file count
+                    int pageCount = FileOperations.GetNumberOfPages(file.FullName);
 
-                        // Get the page count and rename the file
-                        string newFileName = $@"{Path.GetFileNameWithoutExtension(file.FullName)} - [{pageCount}]{Path.GetExtension(file.FullName)}";
+                    // Get the page count and rename the file
+                    string newFileName = $@"{Path.GetFileNameWithoutExtension(file.FullName)} - [{pageCount}]{Path.GetExtension(file.FullName)}";
 
-                        file.MoveTo($@"{FilePath}\{newFileName}");
+                    file.MoveTo($@"{FilePath}\{newFileName}");
 
-                        Console.Clear();
-                        Console.WriteLine($"Files renamed: {counter++}");
-                    }
                 }
             }
-            catch (DirectoryNotFoundException ex)
-            {
-                Console.Clear();
-                Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.StackTrace);
-                Console.WriteLine("\npress any key to exit.");
-                Console.ReadKey(true);
-                Environment.Exit(0 );
-            }
 
-
-
-            WaitForKeyPress();
-
-
+            Console.Clear();
+            NotifyUser($"Files Processed: {counter++}");
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey(true);
+            Environment.Exit(0);
         }
 
-        // Wait for user to press key before continuinig
         private void WaitForKeyPress()
         {
             // Cache the previous color
@@ -90,6 +71,19 @@ namespace PageCounter
             Console.ReadKey(true);
 
             // Reset the color to what it was
+            Console.ForegroundColor = previousColor;
+        }
+
+        private void NotifyUser(string message)
+        {
+            // Cache the previous console foreground color
+            ConsoleColor previousColor = Console.ForegroundColor;
+
+            // Change the color foreground color
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine($"\n{message}");
+
+            // Reset the foreground color back to what it was
             Console.ForegroundColor = previousColor;
         }
 
