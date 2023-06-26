723ff21191f818c7ac0d23060c3cf2be5beecbb9
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-06-26 10:13:59 +0200

    Add 'DirectoryOperations.cs'

---
+++ PageCounter/ConsoleUtils/DirectoryOperations.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace PageCounter.ConsoleUtils
+{
+    internal class DirectoryOperations
+    {
+        public static DirectoryInfo CreateDirectory(string path)
+        {
+            return Directory.CreateDirectory(path);
+        }
+    }
+}
